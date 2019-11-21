d4fc982665d546404fc69da5da3ca5a512d52c0c
DeathstarNovember <johnathon.hill@gmail.com>
2019-11-21 10:25:20 -0500

    envs bugfix

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -1,5 +1,5 @@
 use Mix.Config
 config :legends, Legends.Repo,
   ssl: true,
-  url: database_url,
+  url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
